888bd3f114ad1dcdd73df9d401f2587f783817ee
Purvesh Dodiya <purvesh.d@simformsolutions.com>
2023-04-17 17:47:48 +0530

    Profile setup

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,8 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var firstName: String = ""
+    var lastName: String = ""
+    var Phone_Number: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun SetUpProfileData() {
+        firstName = "Purvesh"
+        lastName = "Doodiya"
+        Phone_Number = "9876543121"
+    }
+
 }
